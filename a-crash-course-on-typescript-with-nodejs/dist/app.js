f9f12fba15d1c9023dcf841d2378785c8da93cbc
adnanrahic <ado.raha198@gmail.com>
2018-01-06 23:00:44 +0100

    added article: A crash course on TypeScript with Node.js

---
+++ a-crash-course-on-typescript-with-nodejs/dist/app.js
@@ -0,0 +1,31 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express = require("express");
+const bodyParser = require("body-parser");
+class App {
+    constructor() {
+        this.app = express();
+        this.config();
+        this.routes();
+    }
+    config() {
+        this.app.use(bodyParser.json());
+        this.app.use(bodyParser.urlencoded({ extended: false }));
+    }
+    routes() {
+        const router = express.Router();
+        router.get('/', (req, res) => {
+            res.status(200).send({
+                message: 'Hello World!'
+            });
+        });
+        router.post('/', (req, res) => {
+            const data = req.body;
+            // query a database and save data
+            res.status(200).send(data);
+        });
+        this.app.use('/', router);
+    }
+}
+exports.default = new App().app;
+//# sourceMappingURL=app.js.map
---
+++ a-crash-course-on-typescript-with-nodejs/lib/server.ts
@@ -0,0 +1,5 @@
+import app from "./app";
+const port = 4040;
+app.listen(port, function() {
+  console.log('Express server listening on port ' + port);
+});
